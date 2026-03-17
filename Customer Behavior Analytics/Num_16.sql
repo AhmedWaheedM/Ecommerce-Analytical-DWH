82286efd1977b898cabae51396dcab50886ef32e
Mariam Taha <mariamtaha2224@gmail.com>
2026-03-17 21:23:01 +0200

    feat: create SQL scripts for Analytical SQL Business Questions

---
+++ Customer Behavior Analytics/Num_16.sql
@@ -0,0 +1,38 @@
+-- 16. Identify the top percentile of high-value customers. 
+
+create or replace view high_value_customers as
+   with customer_revenue as (
+      select f.customer_key,
+             gender,
+             age_group,
+             customer_segment,
+             sum(net_amount) as total_revenue
+        from fact_order_line f
+        join dim_customer d
+      on f.customer_key = d.customer_key
+       group by customer_key
+   ),ranked_customers as (
+      select customer_key,
+             total_revenue,
+             gender,
+             age_group,
+             customer_segment,
+             percent_rank()
+             over(
+                 order by total_revenue
+             ) as percentile_rank
+        from customer_revenue
+   )
+   select *,
+          round(
+             percentile_rank * 100,
+             0
+          ) as percentile_rank,
+          case
+             when percentile_rank >= 0.8 then
+                'Top 20%'
+             else
+                'Other'
+          end as high_customers
+     from high_value_customers
+    order by total_revenue desc;
