32351e43dafb4856f2de068237d3fb0c3a757f72
Mahmoud Shaker El-Rifai <mahmoudshaker.rifai@gmail.com>
2026-03-18 02:11:32 +0200

    feat: refactor time-based queries to views

--- Time-Based Performance Analysis/1- Cumulative Revenue over Time.sql
+++ Time-Based Performance Analysis/1- Cumulative Revenue over Time.sql
@@ -1,8 +1,9 @@
 # 1- Produce cumulative revenue over time to understand long-term growth behavior.
 
-SELECT full_date AS Date, SUM(net_amount) AS "Daily Revenue",
-    SUM(SUM(net_amount)) OVER (ORDER BY d.full_date) AS "Cumulative Revenue"
-FROM fact_order_line o JOIN dim_date d
-ON o.date_key = d.date_key
-GROUP BY full_date
-ORDER BY full_date;
+CREATE OR REPLACE VIEW cumulative_revenue AS
+	SELECT d.full_date AS Date, SUM(o.net_amount) AS "Daily Revenue",
+		SUM(SUM(o.net_amount)) OVER (ORDER BY d.full_date) AS "Cumulative Revenue"
+	FROM fact_order_line o JOIN dim_date d
+	ON o.date_key = d.date_key
+	GROUP BY d.full_date
+	ORDER BY d.full_date;
--- Time-Based Performance Analysis/2- Month-to-Date Performance.sql
+++ Time-Based Performance Analysis/2- Month-to-Date Performance.sql
@@ -1,8 +1,9 @@
 # 2- Measure Month-to-Date performance to evaluate intra-month trends
 
-SELECT full_date AS Date, year, month, SUM(net_amount) AS "Daily Revenue",
-    SUM(SUM(net_amount)) OVER (PARTITION BY year, month ORDER BY d.full_date) AS "MTD Revenue"
-FROM fact_order_line o JOIN dim_date d
-ON o.date_key = d.date_key
-GROUP BY full_date, year, month
-ORDER BY full_date
+CREATE OR REPLACE VIEW MTD_performance AS
+	SELECT d.full_date AS Date, d.year, d.month, SUM(o.net_amount) AS "Daily Revenue",
+		SUM(SUM(o.net_amount)) OVER (PARTITION BY year, month ORDER BY d.full_date) AS "MTD Revenue"
+	FROM fact_order_line o JOIN dim_date d
+	ON o.date_key = d.date_key
+	GROUP BY d.full_date, d.year, d.month
+	ORDER BY d.full_date
