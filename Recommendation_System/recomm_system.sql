23028b124dffd981cda2bcacd2ce24cba2fd7c78
Mariam Taha <mariamtaha2224@gmail.com>
2026-03-22 19:32:57 +0200

    refactor: edit recommendaion query

--- Recommendation_System/recomm_system.sql
+++ Recommendation_System/recomm_system.sql
@@ -1,71 +1,69 @@
 -- Use ecommerce_Dw;
 
 -- check all products in our business
-SELECT 
-    f.order_id,
-    f.product_key,
-    dp.product_name,
-    f.category_key,
-    dc.category_name,
-    f.net_amount,
-    dd.full_date
-FROM fact_order_line f
-JOIN dim_product dp
-    ON f.product_key = dp.product_key
-JOIN dim_category dc
-    ON f.category_key = dc.category_key
-JOIN dim_date dd
-    ON f.date_key = dd.date_key;
+select f.order_id,
+       f.product_key,
+       dp.product_name,
+       f.category_key,
+       dc.category_name,
+       f.net_amount,
+       dd.full_date
+  from fact_order_line f
+  join dim_product dp
+on f.product_key = dp.product_key
+  join dim_category dc
+on f.category_key = dc.category_key
+  join dim_date dd
+on f.date_key = dd.date_key;
 
 
 -- check products bought together in the same order
-WITH product_pairs AS (
-    SELECT 
-        a.product_key AS First_Product,
-        dp.product_name AS First_Product_Name,
-        b.product_key AS Second_Product,
-        dp2.product_name AS Second_Product_Name,
+with product_pairs as (
+   select -- a.product_key as first_product,
+          dp.product_name as first_product_name,
+          dc.category_name as fisrt_category_name,
+          -- b.product_key as second_product,
+          dp2.product_name as second_product_name,
+          dc2.category_name as second_category_name,
 
-        CASE 
+          CASE 
             WHEN a.category_key = b.category_key THEN 'Same Category'
             ELSE 'Different Category'
         END AS Relation_Type
 
-    FROM fact_order_line a
-    JOIN fact_order_line b
-        ON a.order_id = b.order_id
-        AND a.product_key < b.product_key
-        
-
-    JOIN dim_product dp
-        ON a.product_key = dp.product_key
-    JOIN dim_product dp2
-        ON b.product_key = dp2.product_key
+     from fact_order_line a
+     join fact_order_line b
+   on a.order_id = b.order_id
+      and a.product_key < b.product_key
+     join dim_product dp
+   on a.product_key = dp.product_key
+     join dim_product dp2
+   on b.product_key = dp2.product_key
+     join dim_category dc
+   on a.category_key = dc.category_key
+     join dim_category dc2
+   on b.category_key = dc2.category_key
 ),
 -- How many these products bought together
 -- Detect if they in the same category or not
-pair_counts AS (
-    SELECT 
-        First_Product,
-        First_Product_Name,
-        Second_Product,
-        Second_Product_Name,
-        Relation_Type,
-        COUNT(*) AS Frequency
-    FROM product_pairs
-    GROUP BY 
-        First_Product,
-        First_Product_Name,
-        Second_Product,
-        Second_Product_Name,
-        Relation_Type
+pair_counts as (
+   select -- first_product,
+          First_Product_Name,
+          Fisrt_Category_Name,
+          -- second_product,
+          Second_Product_Name,
+          Second_Category_Name,
+          Relation_Type,
+          count(*) as frequency
+     from product_pairs
+    group by -- first_product,
+             First_Product_Name,
+             Fisrt_Category_Name,
+             -- second_product,
+             Second_Product_Name,
+             Second_Category_Name,
+             Relation_Type
 )
-SELECT 
-    *,
-    ROW_NUMBER() OVER (
-        PARTITION BY First_Product
-        ORDER BY Frequency DESC
-    ) AS Rank_Per_Product
-FROM pair_counts
-ORDER BY Frequency DESC;
-
+select *
+  from pair_counts
+ order by frequency desc;
